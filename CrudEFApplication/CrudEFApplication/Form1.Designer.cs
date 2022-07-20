ef4f9708e0eac74b3c25c63d881c6ec7463222f1
jdnlldnvrthdr <84691955+jdnlldnvrthdr@users.noreply.github.com>
2022-07-20 15:10:16 +0800

    Add files via upload

---
+++ CrudEFApplication/CrudEFApplication/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace CrudEFApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxFirstName = new System.Windows.Forms.TextBox();
+            this.txtBoxAdd = new System.Windows.Forms.TextBox();
+            this.txtBoxAge = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.CustomerID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtBoxLastName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Age";
+            // 
+            // txtBoxFirstName
+            // 
+            this.txtBoxFirstName.Location = new System.Drawing.Point(122, 79);
+            this.txtBoxFirstName.Name = "txtBoxFirstName";
+            this.txtBoxFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxFirstName.TabIndex = 3;
+            this.txtBoxFirstName.TextChanged += new System.EventHandler(this.txtBoxFirstName_TextChanged);
+            // 
+            // txtBoxAdd
+            // 
+            this.txtBoxAdd.Location = new System.Drawing.Point(122, 132);
+            this.txtBoxAdd.Name = "txtBoxAdd";
+            this.txtBoxAdd.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxAdd.TabIndex = 4;
+            this.txtBoxAdd.TextChanged += new System.EventHandler(this.txtBoxAdd_TextChanged);
+            // 
+            // txtBoxAge
+            // 
+            this.txtBoxAge.Location = new System.Drawing.Point(122, 158);
+            this.txtBoxAge.Name = "txtBoxAge";
+            this.txtBoxAge.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxAge.TabIndex = 5;
+            this.txtBoxAge.TextChanged += new System.EventHandler(this.txtBoxAge_TextChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(582, 109);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(124, 20);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(582, 135);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(124, 20);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(582, 159);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(124, 20);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.Cancel);
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CustomerID,
+            this.FirstName,
+            this.LastName,
+            this.Address,
+            this.Age});
+            this.dgvData.Location = new System.Drawing.Point(61, 183);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.Size = new System.Drawing.Size(645, 240);
+            this.dgvData.TabIndex = 9;
+            this.dgvData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvData_CellContentClick);
+            this.dgvData.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvData_CellMouseClick);
+            this.dgvData.DoubleClick += new System.EventHandler(this.dgvData_DoubleClick);
+            // 
+            // CustomerID
+            // 
+            this.CustomerID.DataPropertyName = "CustomerID";
+            this.CustomerID.HeaderText = "CustomerID";
+            this.CustomerID.Name = "CustomerID";
+            this.CustomerID.ReadOnly = true;
+            this.CustomerID.Visible = false;
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.FirstName.DataPropertyName = "FirstName";
+            this.FirstName.HeaderText = "First Name";
+            this.FirstName.Name = "FirstName";
+            this.FirstName.ReadOnly = true;
+            // 
+            // LastName
+            // 
+            this.LastName.DataPropertyName = "LastName";
+            this.LastName.HeaderText = "Last Name";
+            this.LastName.Name = "LastName";
+            this.LastName.ReadOnly = true;
+            // 
+            // Address
+            // 
+            this.Address.DataPropertyName = "Address";
+            this.Address.HeaderText = "Address";
+            this.Address.Name = "Address";
+            this.Address.ReadOnly = true;
+            // 
+            // Age
+            // 
+            this.Age.DataPropertyName = "Age";
+            this.Age.HeaderText = "Age";
+            this.Age.Name = "Age";
+            this.Age.ReadOnly = true;
+            // 
+            // txtBoxLastName
+            // 
+            this.txtBoxLastName.Location = new System.Drawing.Point(122, 106);
+            this.txtBoxLastName.Name = "txtBoxLastName";
+            this.txtBoxLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxLastName.TabIndex = 10;
+            this.txtBoxLastName.TextChanged += new System.EventHandler(this.txtBoxLastName_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Last name";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(582, 81);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(124, 23);
+            this.btnNew.TabIndex = 12;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBoxLastName);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtBoxAge);
+            this.Controls.Add(this.txtBoxAdd);
+            this.Controls.Add(this.txtBoxFirstName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxFirstName;
+        private System.Windows.Forms.TextBox txtBoxAdd;
+        private System.Windows.Forms.TextBox txtBoxAge;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.TextBox txtBoxLastName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CustomerID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+        private System.Windows.Forms.Button btnNew;
+    }
+}
+
---
+++ CrudEFApplication/CrudEFApplication/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace CrudEFApplication
+{
+
+    public partial class Form1 : Form
+    {
+        public enum TransactionType
+        {
+            Create,
+            Read,
+            Update,
+            Delete,
+        }
+
+        TransactionType _transactionType = TransactionType.Read;
+
+
+        Customer model = new Customer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel(object sender, EventArgs e)
+        {
+            Cancel();
+        }
+        void Cancel()
+        {
+
+            txtBoxFirstName.Text = txtBoxLastName.Text = txtBoxAdd.Text = txtBoxAge.Text = "";
+
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            model.CustomerID = 0;
+        }
+        void New()
+        {
+            txtBoxFirstName.Text = txtBoxLastName.Text = txtBoxAdd.Text = txtBoxAge.Text = "";
+
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            model.CustomerID = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cancel();
+            this.ActiveControl = txtBoxFirstName;
+            PopulateDataGridView();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            model.FirstName = txtBoxFirstName.Text;
+            model.LastName = txtBoxLastName.Text;
+            model.Address = txtBoxAdd.Text;
+            model.Age = Convert.ToInt32(txtBoxAge.Text);
+
+            using (DBEntities db = new DBEntities())
+
+            {
+                if (String.IsNullOrWhiteSpace(txtBoxFirstName.Text) || String.IsNullOrWhiteSpace(txtBoxLastName.Text) ||
+                    String.IsNullOrWhiteSpace(txtBoxAdd.Text) || String.IsNullOrWhiteSpace(txtBoxAge.Text))
+                {
+                    MessageBox.Show("Required Missing Field!", "EF CRUD Operation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (model.CustomerID == 0/*_transactionType == TransactionType.Create*/)
+                        db.Customers.Add(model);
+                    else
+                        db.Entry(model).State = EntityState.Modified;
+
+                    db.SaveChanges();
+                    MessageBox.Show("Submitted Successfully", "EF CRUD Operation");
+                }
+            }
+            Cancel();
+            PopulateDataGridView();
+        }
+        void PopulateDataGridView()
+        {
+            dgvData.AutoGenerateColumns = false;
+            using (DBEntities db = new DBEntities())
+            {
+                dgvData.DataSource = db.Customers.ToList<Customer>();
+            }
+        }
+        private void dgvData_DoubleClick(object sender, EventArgs e)
+        {
+            _transactionType = TransactionType.Update;
+
+            if (dgvData.CurrentRow.Index != -1)
+            {
+                model.CustomerID = Convert.ToInt32(dgvData.CurrentRow.Cells["CustomerID"].Value);
+                using (DBEntities db = new DBEntities())
+                {
+                    model = db.Customers.Where(x => x.CustomerID == model.CustomerID).FirstOrDefault();
+                    txtBoxFirstName.Text = model.FirstName;
+                    txtBoxLastName.Text = model.LastName;
+                    txtBoxAdd.Text = model.Address;
+                    txtBoxAge.Text = model.Age == null ? "" : model.Age.ToString();
+                    //if (model.Age == null)
+                    //{
+                    //    txtBoxAge.Text = "";
+                    //}
+                    //else
+                    //{
+                    //    txtBoxAge.Text = model.Age.ToString();
+                    //}
+                }
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            _transactionType = TransactionType.Delete;
+
+            if (MessageBox.Show("Are you sure to delete this Data?", "EF CRUD Operation",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                using (DBEntities db = new DBEntities())
+                {
+                    var customer = db.Customers.FirstOrDefault(d => d.CustomerID == model.CustomerID);
+                    db.Customers.Remove(customer);
+                    db.SaveChanges();
+
+                    /*var entry = db.Entry(model);
+                    if (entry.State == EntityState.Detached)
+                        db.Customers.Attach(model);
+                    db.Customers.Remove(model);
+
+                    db.SaveChanges();*/
+                    PopulateDataGridView();
+                    Cancel();
+
+                    MessageBox.Show("Deleted Succesfully", "EF CRUD Operation");
+                }
+        }
+        private void txtBoxFirstName_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtBoxFirstName.Text, "^[a-zA-Z]+$"))
+            {
+            }
+            else
+            {
+                txtBoxFirstName.Text = txtBoxFirstName.Text.Remove(txtBoxFirstName.Text.Length - 1);    
+                MessageBox.Show("Enter only Alphabets", "EF CRUD Operation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+        private void txtBoxAge_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtBoxAge.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers", "EF CRUD Operation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBoxAge.Text = txtBoxAge.Text.Remove(txtBoxAge.Text.Length - 1);
+            }
+
+
+        }
+        private void txtBoxLastName_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtBoxLastName.Text, "^[a-zA-Z]+$"))
+            {
+            }
+            else
+            {
+                txtBoxLastName.Text = txtBoxLastName.Text.Remove(txtBoxLastName.Text.Length - 1);
+                MessageBox.Show("Enter only Alphabets", "EF CRUD Operation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+            }
+        }
+        private void txtBoxAdd_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void dgvData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+        private void dgvData_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            _transactionType = TransactionType.Update;
+            if (dgvData.CurrentRow.Index != -1)
+            {
+                model.CustomerID = Convert.ToInt32(dgvData.CurrentRow.Cells["CustomerID"].Value);
+                using (DBEntities db = new DBEntities())
+                {
+                    model = db.Customers.Where(x => x.CustomerID == model.CustomerID).FirstOrDefault();
+                    txtBoxFirstName.Text = model.FirstName;
+                    txtBoxLastName.Text = model.LastName;
+                    txtBoxAdd.Text = model.Address;
+                    txtBoxAge.Text = model.Age.ToString();
+                    //if (model.Age == null)
+                    //{
+                    //    txtBoxAge.Text = "";
+                    //}
+                    //else
+                    //{
+                    //    txtBoxAge.Text = model.Age.ToString();
+                    //}
+                }
+                btnSave.Text = "Update";
+                btnCancel.Text = "Cancel";
+                btnDelete.Enabled = true;
+
+            }
+        }
+
+
+        private void btnNew_Click_1(object sender, EventArgs e)
+        {
+
+            New();
+
+            {
+
+            }
+        }
+    }
+}
